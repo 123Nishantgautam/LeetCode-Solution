30bc3fcf70d086d38ef539e4ae07f43ac45c4a1e
123Nishantgautam <123.nishantgautam@gmail.com>
2026-09-14 22:56:46 +0530

    Added Minimum Size of Subarray Sum solution

---
+++ Array/Min_Size_SubArray_sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int minLen = INT_MAX;
+        int sum = 0;
+        int left = 0; 
+
+        for(int right = 0; right<nums.size(); right++){
+            sum = sum + nums[right];
+            while(sum >= target){
+                int curLen = right - left + 1;
+                minLen = min(minLen, curLen);
+                sum = sum - nums[left];
+                left++;
+            }
+        }
+        if(minLen == INT_MAX){
+            return 0;
+        }
+
+        return minLen;
+    }
+};
