4d1d9b48aa09b666da205319e6802e8e157e2b69
123Nishantgautam <123.nishantgautam@gmail.com>
2026-09-22 21:48:59 +0530

    Added Contains_Duplicate solution

---
+++ Array/Contains_Duplicate.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> seen;
+
+        for(int num : nums){
+            if(seen.count(num)){
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
+    }
+};
